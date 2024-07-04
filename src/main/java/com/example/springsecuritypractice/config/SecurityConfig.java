b68e3e5646fa6f077707d316ee7966a8f0c732e3
jhj1819 <72265381+jhj1819@users.noreply.github.com>
2024-07-05 00:51:04 +0900

    UPDATE JoinService

--- src/main/java/com/example/springsecuritypractice/config/SecurityConfig.java
+++ src/main/java/com/example/springsecuritypractice/config/SecurityConfig.java
@@ -23,7 +23,7 @@ public class SecurityConfig {
 
         http
                 .authorizeHttpRequests((auth) -> auth
-                        .requestMatchers("/", "/login").permitAll() //메인페이지, login화면에 모두 접근 가능
+                        .requestMatchers("/", "/login", "/join", "/joinProcess").permitAll() //메인페이지, login화면에 모두 접근 가능
                         .requestMatchers("/admin").hasRole("ADMIN") // ADMIN 권한이 있을때만 접근하도록
                         .requestMatchers("/my/**").hasAnyRole("ADMIN", "USER") // 여러개의 권한이 있을때, 사용자 ID가 다를 수 있으므로 **와일드카드 사용
                         .anyRequest().authenticated() //나머지 경우 로그인한 사용자만 가능하도록
--- src/main/java/com/example/springsecuritypractice/service/JoinService.java
+++ src/main/java/com/example/springsecuritypractice/service/JoinService.java
@@ -1,12 +1,32 @@
 package com.example.springsecuritypractice.service;
 
 import com.example.springsecuritypractice.dto.JoinDTO;
+import com.example.springsecuritypractice.entity.UserEntity;
+import com.example.springsecuritypractice.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class JoinService {
 
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
     public void joinProcess(JoinDTO joinDTO){
+
+        //db에 이미 동일한 username을 가진 사람이 있는지 존재하는지?
+
+        UserEntity data = new UserEntity();
+
+        data.setUsername(joinDTO.getUsername());
+        data.setPassword(bCryptPasswordEncoder.encode(joinDTO.getPassword()));
+        data.setRole("ROLE_USER");
+
+        userRepository.save(data);
 
     }
 }
