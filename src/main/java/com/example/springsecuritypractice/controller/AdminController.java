91ecdaafc677fa442c780ad406fa09f6a569a809
jhj1819 <72265381+jhj1819@users.noreply.github.com>
2024-07-05 23:02:54 +0900

    ADD Login

--- src/main/java/com/example/springsecuritypractice/controller/AdminController.java
+++ src/main/java/com/example/springsecuritypractice/controller/AdminController.java
@@ -1,7 +1,9 @@
 package com.example.springsecuritypractice.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
 public class AdminController {
 
     @GetMapping("/admin")
--- src/main/java/com/example/springsecuritypractice/service/CustomUserDetailsService.java
+++ src/main/java/com/example/springsecuritypractice/service/CustomUserDetailsService.java
@@ -1,5 +1,9 @@
 package com.example.springsecuritypractice.service;
 
+import com.example.springsecuritypractice.dto.CustomUserDetails;
+import com.example.springsecuritypractice.entity.UserEntity;
+import com.example.springsecuritypractice.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -8,9 +12,18 @@ import org.springframework.stereotype.Service;
 @Service
 public class CustomUserDetailsService implements UserDetailsService {
 
+    @Autowired
+    private UserRepository userRepository;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        UserEntity userData = userRepository.findByUsername(username);
+
+        if (userData != null) {
+
+            return new CustomUserDetails(userData);
+        }
+
         return null;
     }
 }
