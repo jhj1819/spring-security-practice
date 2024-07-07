d8091ac427b65cde4f61101669b1b722d809c667
jhj1819 <72265381+jhj1819@users.noreply.github.com>
2024-07-07 19:17:57 +0900

    REFACTOR: Switch authentication method from form login to HTTP Basic

--- src/main/java/com/example/springsecuritypractice/config/SecurityConfig.java
+++ src/main/java/com/example/springsecuritypractice/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.example.springsecuritypractice.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -28,13 +29,15 @@ public class SecurityConfig {
                         .requestMatchers("/my/**").hasAnyRole("ADMIN", "USER") // 여러개의 권한이 있을때, 사용자 ID가 다를 수 있으므로 **와일드카드 사용
                         .anyRequest().authenticated() //나머지 경우 로그인한 사용자만 가능하도록
                 );
-
+/*
         http
                 .formLogin((auth) -> auth.loginPage("/login")
                         .loginProcessingUrl("/loginProcess")
                         .permitAll()
                 );
-
+*/
+        http
+                .httpBasic(Customizer.withDefaults());
         //http
         //        .csrf((auto) -> auto.disable());
 
